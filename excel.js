8c30144cfb86f610f28b4da805e4bd1f2c16738a
akashaswal5 <aswalakash072@gmail.com>
2026-09-25 01:07:38 +0530

    feat: add Excel web editor adapter

---
+++ excel.js
@@ -0,0 +1,31 @@
+(() => {
+    const EVENT_NAME = "motiongrid-excel-key";
+    const script = document.createElement("script");
+    script.src = chrome.runtime.getURL("excel_page_script.js");
+    document.documentElement.append(script);
+    script.remove();
+
+    function key(keyName, modifiers = {}) {
+        window.dispatchEvent(new CustomEvent(EVENT_NAME, { detail: { key: keyName, modifiers } }));
+    }
+
+    const isMac = /Mac/.test(navigator.platform || navigator.userAgent);
+    const primary = isMac ? "metaKey" : "ctrlKey";
+    const controller = new window.MotionGridModal.ModalController({
+        label: "EXCEL",
+        key,
+        command(name) {
+            const shortcut = { copy: "c", cut: "x", paste: "v", undo: "z", redo: "y", find: "f" }[name];
+            if (shortcut) key(shortcut, { [primary]: true });
+        },
+        insert(after) {
+            if (after) key("End");
+            key("F2");
+        },
+        openLine(above) {
+            key("End");
+            key("Enter", { shiftKey: above });
+        },
+    });
+    document.addEventListener("keydown", (event) => controller.handle(event), true);
+})();
